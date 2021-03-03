2ebc85b9c9337026aae4adc2c0eb73f8058e79e2
willwardlow <wardlow.william@gmail.com>
2021-03-03 10:23:50 -0600

    Created .jsx files for each screen and imported to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import { Route, Switch } from 'react-router-dom';
-import Home from '../screens/Home/Home';
-import PostCreate from '../screens/PostCreate/PostCreate';
-import PostDetail from '../screens/PostDetail/PostDetail';
-import PostEdit from '../screens/PostEdit/PostEdit';
+import Home from './screens/Home/Home.jsx';
+import PostCreate from './screens/PostCreate/PostCreate.jsx';
+import PostDetail from './screens/PostDetail/PostDetail.jsx';
+import PostEdit from './screens/PostEdit/PostEdit.jsx';
 import './App.css';
 
 function App() {
