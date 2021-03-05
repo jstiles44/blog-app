64d0c4dae23eb7906782a54bd50d7c1f262002a6
Jacob Stiles <jacobstiles@Jacobs-MacBook-Pro.local>
2021-03-05 10:29:39 -0500

    nav bar is now responsive

--- client/src/components/shared/Nav/Nav.jsx
+++ client/src/components/shared/Nav/Nav.jsx
@@ -1,20 +1,32 @@
 import "./Nav.css";
 import { NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Nav = () => {
+  const [width, setWidth] = useState(window.innerWidth)
+  
+
+  useEffect(() => {
+    const handleResizeWindow = () => setWidth(window.innerWidth)
+    window.addEventListener("resize", handleResizeWindow)
+  })
   return (
     <nav className="nav">
       <div className="logo-container">
       <img className="kiwi" src="https://i.ibb.co/cL8wtdc/kiwi-slice.png" alt-text="kiwi" />
-        <div className="logo">LymeWyre Music Reviews</div>
+        <div className="logo">LymeWyre Reviews</div>
         </div>
       <div className="links">
+        
         <NavLink className="link" to="/">
-          Home
+          {width > 560 ? "Home":"🏠"}
         </NavLink>
+        
+        
         <NavLink className="link" to="/new">
-          Add Review
-        </NavLink>
+          {width > 560 ? "Add Review":"➕"}
+          </NavLink>
+          
       </div>
     </nav>
   );
